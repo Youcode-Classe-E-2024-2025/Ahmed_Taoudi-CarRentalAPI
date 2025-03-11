62473eb29dbb412425a24e200f78f11b168e34f1
Tawdi <ahmedtawdi00@gmail.com>
2025-03-11 11:42:45 +0000

    ajoute de route pour rentals

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\RentalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,7 @@ Route::prefix('auth')
     );
 
 Route::apiResource('cars', CarController::class);
+Route::apiResource('rentals', RentalController::class);
+
+Route::get('users/{userId}/rentals', [RentalController::class, 'rentalsByUser']);
+Route::get('cars/{carId}/rentals', [RentalController::class, 'rentalsByCar']);
